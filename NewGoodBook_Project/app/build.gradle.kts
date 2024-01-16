62a85984456d9301c23275d93763d19985a8d6ea
SilviaCC1701 <jesus33lcc@gmail.com>
2024-01-16 08:36:32 +0100

    Configuracion de Fragments Provisional

--- NewGoodBook_Project/app/build.gradle.kts
+++ NewGoodBook_Project/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,11 +39,11 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
 }
 
--- NewGoodBook_Project/app/src/main/java/com/example/newgoodbooks/Principal.java
+++ NewGoodBook_Project/app/src/main/java/com/example/newgoodbooks/Principal.java
@@ -1,14 +1,52 @@
 package com.example.newgoodbooks;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.newgoodbooks.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Principal extends AppCompatActivity {
+    LibroFragment libroFragment=new LibroFragment();
+    ExplorarFragment explorarFragment=new ExplorarFragment();
+    ListasFragment listasFragment=new ListasFragment();
+    BottomNavigationView menuNavegacion;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        frameLayout=findViewById(R.id.frameContainer);
+        menuNavegacion=findViewById(R.id.menuNavegacion);
+        menuNavegacion.setOnItemSelectedListener(item -> {
+           switch (item.getItemId()){
+               case R.id.itemPrincipal:
+                   reemplazarFragment(libroFragment);
+                   break;
+               case R.id.itemExplorar:
+                   reemplazarFragment(explorarFragment);
+                   break;
+               case R.id.itemListas:
+                   reemplazarFragment(listasFragment);
+                   break;
+           }
+           return false;
+        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameContainer, libroFragment).commit();
+    }
+    private void reemplazarFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameContainer, fragment);
+        fragmentTransaction.commit();
     }
 }
